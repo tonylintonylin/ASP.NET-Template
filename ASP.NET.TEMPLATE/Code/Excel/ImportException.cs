1811fdbf257ff38863fc1fa57efe1fa39c96ccf4
Tony Lin <tonylinbusiness@gmail.com>
2020-12-03 13:15:02 -0500

    Database seeded with template

---
+++ ASP.NET.TEMPLATE/Code/Excel/ImportException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ASP.NET.TEMPLATE
+{
+    public class ImportException : Exception
+    {
+        public ImportException(string message, Exception ex)
+            : base(message, ex)
+        {
+        }
+    }
+}
---
+++ ASP.NET.TEMPLATE/Domain/User.partial.cs
@@ -0,0 +1,7 @@
+﻿namespace ASP.NET.TEMPLATE.Domain
+{
+    public partial class User
+    {
+        public string Name { get { return FirstName + " " + LastName; } }
+    }
+}
